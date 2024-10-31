aafbabc4bfbbc1c99d4d5c543bacb8a7c29f6c6e
popmanpop <zsasha957@gmail.com>
2024-10-31 18:53:55 +0300

    Modified some stuff

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ import matplotlib.pyplot as plt
 
 startT = int(input("Введите начальную температуру: "))
 plavT = int(input("Введите температуру плавления: "))
-C = int(input("Введите удельную теплоёмкость: "))
-lamb1 = int(input("Введите первую часть удельной теплоты плавления: "))
-lamb2 = int(input("Введите степень удельной теплоты плавления: "))
-mass = int(input("Введите массу: "))
- 
-lamb = lamb1 ** lamb2
-Qcount = 0
-#dumbNumber = 10000000000 ** 100000000000000
+C = int(input("Введите удельную теплоёмкость(Дж\Кг): "))
+lamb = int(input("Введите удельную теплоту плавления(Дж\Кг): "))
+mass = int(input("Введите массу(Кг): "))
+
+
 x = []
 y = []
 
 def Qcal(endQ, endT, lamMultMass, x: list , y: list):
-    for q in range(endQ, lamMultMass, C):
-        x.append(q)
-        y.append(endT)
+    for q in range(endQ, lamMultMass):
+        if q + 1 == lamMultMass:
+            x.append(q)
+            y.append(endT)
+        if q == endQ:
+            x.append(q)
+            y.append(endT)
 
 for temp in range(startT, plavT+1):
 
     deltaT = temp - startT
     Q = C * mass * deltaT
-
-    if temp >= plavT:
-        Qcal(Q, temp, Q * lamb, x , y)
-        break
-    else:
+    
+    if temp == startT:
         x.append(Q)
         y.append(temp)
+    if temp + 1 == plavT + 1:
+        x.append(Q)
+        y.append(temp)
+    if temp >= plavT:
+        Qcal(Q, temp, mass * lamb, x , y)
+        break
+    
     
 
 plt.plot(x, y)
